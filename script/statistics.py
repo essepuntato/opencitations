815d7f99e8f34d83aa3dc995aadc43705120a206
S <essepuntato@gmail.com>
2016-07-20 18:02:13 +0200

    Use of SPARQLWrapper in statistics

--- script/statistics.py
+++ script/statistics.py
@@ -2,62 +2,60 @@
 # -*- coding: utf-8 -*-
 __author__ = 'essepuntato'
 
-from rdflib import ConjunctiveGraph
+import json
 from conf_spacin import *
 from datetime import datetime
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 queries = [
     """
     PREFIX fabio: <http://purl.org/spar/fabio/>
     PREFIX cito: <http://purl.org/spar/cito/>
     SELECT (count(?citing) as ?tot) {
-      ?citing a fabio:Expression .
-      FILTER EXISTS { ?citing cito:cites [] }
-    }""",
-    """
-    PREFIX fabio: <http://purl.org/spar/fabio/>
-    PREFIX cito: <http://purl.org/spar/cito/>
-    SELECT (count(?cited) as ?tot) {
-      ?cited a fabio:Expression .
-      FILTER EXISTS { ?cited ^cito:cites [] }
+        GRAPH <https://w3id.org/oc/corpus/br/> {
+            ?citing a fabio:Expression .
+            FILTER EXISTS { ?citing cito:cites | ^cito:cites [] }
+      }
     }""",
     """
     PREFIX cito: <http://purl.org/spar/cito/>
     SELECT (count(?citing) as ?tot) {
-        ?citing cito:cites ?cited
+        GRAPH <https://w3id.org/oc/corpus/br/> {
+            ?citing cito:cites ?cited
+        }
     }""",
     """
     PREFIX fabio: <http://purl.org/spar/fabio/>
     PREFIX frbr: <http://purl.org/vocab/frbr/core#>
     SELECT (count(DISTINCT ?container) as ?tot) {
-        ?container a fabio:Expression ; ^frbr:partOf ?something .
-        FILTER NOT EXISTS { ?container frbr:partOf ?other_container }
-    }""",
-    """
-    PREFIX pro: <http://purl.org/spar/pro/>
-    SELECT (count(DISTINCT ?publisher) as ?tot) {
-        ?role pro:withRole pro:publisher ; pro:isHeldBy ?publisher
-    }""",
-    """
-    PREFIX pro: <http://purl.org/spar/pro/>
-    SELECT (count(?author) as ?tot) {
-        ?role pro:withRole pro:author ; pro:isHeldBy ?author
+        GRAPH <https://w3id.org/oc/corpus/br/> {
+            ?container ^frbr:partOf ?something .
+            FILTER NOT EXISTS { ?container frbr:partOf ?other_container }
+        }
     }""",
     """
     PREFIX datacite: <http://purl.org/spar/datacite/>
     PREFIX pro: <http://purl.org/spar/pro/>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    SELECT (count(DISTINCT ?author) as ?tot) {
-      ?author a foaf:Agent ; datacite:hasIdentifier []
+    SELECT (count(?id) as ?tot) {
+        GRAPH <https://w3id.org/oc/corpus/id/> {
+            ?id datacite:usesIdentifierScheme datacite:orcid
+        }
     }"""
 ]
 
-ts = ConjunctiveGraph('SPARQLUpdateStore')
-ts.open((triplestore_url, triplestore_url))
+tp = SPARQLWrapper(triplestore_url)
+tp.setMethod('GET')
 
 res = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 for query in queries:
-    for tot, in ts.query(query):
-        res += "," + str(tot)
+    print "WRAP", query
+    tp.setQuery(query)
+    tp.setReturnFormat(JSON)
+    results = json.loads(tp.query().convert())
+
+    for result in results["results"]["bindings"]:
+        res += "," + result["tot"]["value"]
+
 with open(base_home + "statistics.csv", "a") as f:
     f.write(res + "\n")
