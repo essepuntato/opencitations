e48b97d4060d48c5747ca1fd0955816ad7001fbe
S <essepuntato@gmail.com>
2016-07-27 12:21:38 +0200

    sleep fix backup

--- backup.sh
+++ backup.sh
@@ -80,19 +80,27 @@ cd /srv/oc
 
 CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus' &"
 eval $CMD
+sleep 2
 dar -c - -R corpus | nc -w 1 130.136.2.21 5000
+sleep 5
 
 CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-id-counter' &"
 eval $CMD
+sleep 2
 dar -c - -R id-counter | nc -w 1 130.136.2.21 5000
+sleep 5
 
 CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-ref' &"
 eval $CMD
+sleep 2
 dar -c - -R ref | nc -w 1 130.136.2.21 5000
+sleep 5
 
 CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-triplestore' &"
 eval $CMD
+sleep 2
 dar -c - -R triplestore -X "log.txt" -X "nohup.out" | nc -w 1 130.136.2.21 5000
+sleep 5
 
 # Gently sun all the processes
 /home/essepuntato/OC/script/gently-run.sh
