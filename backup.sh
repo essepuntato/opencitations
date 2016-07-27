bc2d72f9ddb5bdf850d8789da9d659d18909affa
S <essepuntato@gmail.com>
2016-07-27 02:31:58 +0200

    new backup script with partial dar in occ

--- backup.sh
+++ backup.sh
@@ -15,10 +15,62 @@ done
 cd /srv/oc
 
 # Backup of the OCC
-dar -c - -R corpus | ssh 130.136.2.21 -l oc 'dar_xform -s 600M -w -n - /mnt/backup/oc/corpus_`date -I`'
-dar -c - -R id-counter | ssh 130.136.2.21 -l oc 'dar_xform -s 600M -w -n - /mnt/backup/oc/id-counter_`date -I`'
-dar -c - -R ref | ssh 130.136.2.21 -l oc 'dar_xform -s 600M -w -n - /mnt/backup/oc/ref_`date -I`'
-dar -c - -R triplestore -X "log.txt" -X "nohup.out" | ssh 130.136.2.21 -l oc 'dar_xform -s 600M -w -n - /mnt/backup/oc/triplestore_`date -I`'
+cd corpus
+
+cd ar
+for d in */ ; do
+    CUR_DIR=${d%?}
+    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ar_$CUR_DIR'"
+    eval $CMD
+done
+cd ..
+
+cd be
+for d in */ ; do
+    CUR_DIR=${d%?}
+    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_be_$CUR_DIR'"
+    eval $CMD
+done
+cd ..
+
+cd br
+for d in */ ; do
+    CUR_DIR=${d%?}
+    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_br_$CUR_DIR'"
+    eval $CMD
+done
+cd ..
+
+cd id
+for d in */ ; do
+    CUR_DIR=${d%?}
+    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_id_$CUR_DIR'"
+    eval $CMD
+done
+cd ..
+
+cd ra
+for d in */ ; do
+    CUR_DIR=${d%?}
+    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ra_$CUR_DIR'"
+    eval $CMD
+done
+cd ..
+
+cd re
+for d in */ ; do
+    CUR_DIR=${d%?}
+    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_re_$CUR_DIR'"
+    eval $CMD
+done
+cd ..
+
+CMD="dar -c - -R prov | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_prov'"
+eval $CMD
+
+dar -c - -R id-counter | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/id-counter_`date -I`'
+dar -c - -R ref | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/ref_`date -I`'
+dar -c - -R triplestore -X "log.txt" -X "nohup.out" | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/triplestore_`date -I`'
 
 # Gently sun all the processes
 /home/essepuntato/OC/script/gently-run.sh
