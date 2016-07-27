5ad605ea7ce62337ac991edd73866bdad29c6e90
S <essepuntato@gmail.com>
2016-07-27 12:18:58 +0200

    new backup

--- backup.sh
+++ backup.sh
@@ -15,62 +15,84 @@ done
 cd /srv/oc
 
 # Backup of the OCC
-cd corpus
+#cd corpus
+#
+#cd ar
+#for d in */ ; do
+#    CUR_DIR=${d%?}
+#    CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ar_$CUR_DIR' &"
+#    eval $CMD
+#    dar -c - -R $CUR_DIR | nc -w 1 130.136.2.21 5000
+#done
+#cd ..
+#
+#cd be
+#for d in */ ; do
+#    CUR_DIR=${d%?}
+#    CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_be_$CUR_DIR' &"
+#    eval $CMD
+#    dar -c - -R $CUR_DIR | nc -w 1 130.136.2.21 5000
+#done
+#cd ..
+#
+#cd br
+#for d in */ ; do
+#    CUR_DIR=${d%?}
+#    CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_br_$CUR_DIR' &"
+#    eval $CMD
+#    dar -c - -R $CUR_DIR | nc -w 1 130.136.2.21 5000
+#done
+#cd ..
+#
+#cd id
+#for d in */ ; do
+#    CUR_DIR=${d%?}
+#    CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_id_$CUR_DIR' &"
+#    eval $CMD
+#    dar -c - -R $CUR_DIR | nc -w 1 130.136.2.21 5000
+#done
+#cd ..
+#
+#cd ra
+#for d in */ ; do
+#    CUR_DIR=${d%?}
+#    CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ra_$CUR_DIR' &"
+#    eval $CMD
+#    dar -c - -R $CUR_DIR | nc -w 1 130.136.2.21 5000
+#done
+#cd ..
+#
+#cd re
+#for d in */ ; do
+#    CUR_DIR=${d%?}
+#    CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_re_$CUR_DIR' &"
+#    eval $CMD
+#    dar -c - -R $CUR_DIR | nc -w 1 130.136.2.21 5000
+#done
+#cd ..
+#
+#CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_prov' &"
+#eval $CMD
+#dar -c - -R prov | nc -w 1 130.136.2.21 5000
+#
+#cd ..
 
-cd ar
-for d in */ ; do
-    CUR_DIR=${d%?}
-    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ar_$CUR_DIR'"
-    eval $CMD
-done
-cd ..
 
-cd be
-for d in */ ; do
-    CUR_DIR=${d%?}
-    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_be_$CUR_DIR'"
-    eval $CMD
-done
-cd ..
-
-cd br
-for d in */ ; do
-    CUR_DIR=${d%?}
-    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_br_$CUR_DIR'"
-    eval $CMD
-done
-cd ..
-
-cd id
-for d in */ ; do
-    CUR_DIR=${d%?}
-    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_id_$CUR_DIR'"
-    eval $CMD
-done
-cd ..
-
-cd ra
-for d in */ ; do
-    CUR_DIR=${d%?}
-    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ra_$CUR_DIR'"
-    eval $CMD
-done
-cd ..
-
-cd re
-for d in */ ; do
-    CUR_DIR=${d%?}
-    CMD="dar -c - -R $CUR_DIR | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_re_$CUR_DIR'"
-    eval $CMD
-done
-cd ..
-
-CMD="dar -c - -R prov | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_prov'"
+CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus' &"
 eval $CMD
+dar -c - -R corpus | nc -w 1 130.136.2.21 5000
 
-dar -c - -R id-counter | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/id-counter_`date -I`'
-dar -c - -R ref | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/ref_`date -I`'
-dar -c - -R triplestore -X "log.txt" -X "nohup.out" | ssh 130.136.2.21 -l oc 'dar_xform -s 500M -w -n - /mnt/backup/oc/triplestore_`date -I`'
+CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-id-counter' &"
+eval $CMD
+dar -c - -R id-counter | nc -w 1 130.136.2.21 5000
+
+CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-ref' &"
+eval $CMD
+dar -c - -R ref | nc -w 1 130.136.2.21 5000
+
+CMD="ssh 130.136.2.21 -l oc 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-triplestore' &"
+eval $CMD
+dar -c - -R triplestore -X "log.txt" -X "nohup.out" | nc -w 1 130.136.2.21 5000
 
 # Gently sun all the processes
 /home/essepuntato/OC/script/gently-run.sh
