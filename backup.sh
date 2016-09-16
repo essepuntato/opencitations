10b28ad6d9c3e145e0ab708ec71c5f05c70454ce
S <essepuntato@gmail.com>
2016-09-16 22:39:50 +0200

    Backup updated with more simple dar commands

--- backup.sh
+++ backup.sh
@@ -13,8 +13,6 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 
-
-INITIAL_DIR="${pwd}"
 BACKUP_DATE="$(date -I)"
 
 # Gently stop all the processes
@@ -29,193 +27,93 @@ done
 # Get last statistics
 /home/essepuntato/OC/statistics.sh
 
-cd /srv/oc
-
 # Backup of the OCC
-cd corpus
 
-cd ar
-for d in */ ; do
-    CUR_DIR=${d%?}
-
-    # backup data
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ar_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    dar -c - -R $CUR_DIR -X index.json -P "./[0-9]*/" | nc -w 1 130.136.2.21 5000
-    sleep 3
-
-    # backup provenance
-    PROV_DIRS=""
-    for p in $CUR_DIR/*/ ; do
-        PROV_DIR=$(echo ${p%?} | rev | cut -d/ -f1)
-        PROV_DIRS="$PROV_DIRS -g '$PROV_DIR/'"
-    done
-    echo $PROV_DIRS
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus_ar_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    DAR="dar -c - -R $CUR_DIR $PROV_DIRS | nc -w 1 130.136.2.21 5000"
-    eval $DAR
-    sleep 3
-done
-cd ..
-
-cd be
-for d in */ ; do
-    CUR_DIR=${d%?}
-
-    # backup data
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_be_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    dar -c - -R $CUR_DIR -X index.json -P "./[0-9]*/" | nc -w 1 130.136.2.21 5000
-    sleep 3
-
-    # backup provenance
-    PROV_DIRS=""
-    for p in $CUR_DIR/*/ ; do
-        PROV_DIR=$(echo ${p%?} | rev | cut -d/ -f1)
-        PROV_DIRS="$PROV_DIRS -g '$PROV_DIR/'"
-    done
-    echo $PROV_DIRS
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus_be_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    DAR="dar -c - -R $CUR_DIR $PROV_DIRS | nc -w 1 130.136.2.21 5000"
-    eval $DAR
-    sleep 3
-done
-cd ..
-
-cd br
-for d in */ ; do
-    CUR_DIR=${d%?}
-
-    # backup data
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_br_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    dar -c - -R $CUR_DIR -X index.json -P "./[0-9]*/" | nc -w 1 130.136.2.21 5000
-    sleep 3
-
-    # backup provenance
-    PROV_DIRS=""
-    for p in $CUR_DIR/*/ ; do
-        PROV_DIR=$(echo ${p%?} | rev | cut -d/ -f1)
-        PROV_DIRS="$PROV_DIRS -g '$PROV_DIR/'"
-    done
-    echo $PROV_DIRS
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus_br_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    DAR="dar -c - -R $CUR_DIR $PROV_DIRS | nc -w 1 130.136.2.21 5000"
-    eval $DAR
-    sleep 3
-done
-cd ..
-
-cd id
-for d in */ ; do
-    CUR_DIR=${d%?}
-
-    # backup data
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_id_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    dar -c - -R $CUR_DIR -X index.json -P "./[0-9]*/" | nc -w 1 130.136.2.21 5000
-    sleep 3
-
-    # backup provenance
-    PROV_DIRS=""
-    for p in $CUR_DIR/*/ ; do
-        PROV_DIR=$(echo ${p%?} | rev | cut -d/ -f1)
-        PROV_DIRS="$PROV_DIRS -g '$PROV_DIR/'"
-    done
-    echo $PROV_DIRS
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus_id_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    DAR="dar -c - -R $CUR_DIR $PROV_DIRS | nc -w 1 130.136.2.21 5000"
-    eval $DAR
-    sleep 3
-done
-cd ..
-
-cd ra
-for d in */ ; do
-    CUR_DIR=${d%?}
-
-    # backup data
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ra_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    dar -c - -R $CUR_DIR -X index.json -P "./[0-9]*/" | nc -w 1 130.136.2.21 5000
-    sleep 3
-
-    # backup provenance
-    PROV_DIRS=""
-    for p in $CUR_DIR/*/ ; do
-        PROV_DIR=$(echo ${p%?} | rev | cut -d/ -f1)
-        PROV_DIRS="$PROV_DIRS -g '$PROV_DIR/'"
-    done
-    echo $PROV_DIRS
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus_ra_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    DAR="dar -c - -R $CUR_DIR $PROV_DIRS | nc -w 1 130.136.2.21 5000"
-    eval $DAR
-    sleep 3
-done
-cd ..
-
-cd re
-for d in */ ; do
-    CUR_DIR=${d%?}
-
-    # backup data
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_re_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    dar -c - -R $CUR_DIR -X index.json -P "./[0-9]*/" | nc -w 1 130.136.2.21 5000
-    sleep 3
-
-    # backup provenance
-    PROV_DIRS=""
-    for p in $CUR_DIR/*/ ; do
-        PROV_DIR=$(echo ${p%?} | rev | cut -d/ -f1)
-        PROV_DIRS="$PROV_DIRS -g '$PROV_DIR/'"
-    done
-    echo $PROV_DIRS
-    CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus_re_$CUR_DIR' &"
-    eval $CMD
-    sleep 2
-    DAR="dar -c - -R $CUR_DIR $PROV_DIRS | nc -w 1 130.136.2.21 5000"
-    eval $DAR
-    sleep 3
-done
-cd ..
-
-CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus' &"
+# ar
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ar' &"
 eval $CMD
 sleep 2
-dar -c - -R prov | nc -w 1 130.136.2.21 5000
+# backup data
+dar -c - -R /srv/oc/corpus/ar/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+# backup provenance
+dar -c - -R /srv/oc/corpus/ar/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
-cd ..
-
-CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-ref' &"
+# be
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_be' &"
 eval $CMD
 sleep 2
-dar -c - -R ref | nc -w 1 130.136.2.21 5000
+# backup data
+dar -c - -R /srv/oc/corpus/be/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+# backup provenance
+dar -c - -R /srv/oc/corpus/be/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
-CMD="ssh 130.136.2.21 -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-triplestore' &"
+# br
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_br' &"
 eval $CMD
 sleep 2
-dar -c - -R triplestore -X "log.txt" -X "nohup.out" | nc -w 1 130.136.2.21 5000
+# backup data
+dar -c - -R /srv/oc/corpus/br/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+# backup provenance
+dar -c - -R /srv/oc/corpus/br/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+
+# id
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_id' &"
+eval $CMD
+sleep 2
+# backup data
+dar -c - -R /srv/oc/corpus/id/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+# backup provenance
+dar -c - -R /srv/oc/corpus/id/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+
+# ra
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ra' &"
+eval $CMD
+sleep 2
+# backup data
+dar -c - -R /srv/oc/corpus/ra/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+# backup provenance
+dar -c - -R /srv/oc/corpus/ra/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+
+# re
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_re' &"
+eval $CMD
+sleep 2
+# backup data
+dar -c - -R /srv/oc/corpus/re/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+# backup provenance
+dar -c - -R /srv/oc/corpus/re/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+
+# prov
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus' &"
+eval $CMD
+sleep 2
+dar -c - -R prov | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+
+# references (done and todo)
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-ref' &"
+eval $CMD
+sleep 2
+dar -c - -R ref | nc -w 1 ocbackup.cs.unibo.it 5000
+sleep 3
+
+# triplestore
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-triplestore' &"
+eval $CMD
+sleep 2
+dar -c - -R triplestore -X "log.txt" -X "nohup.out" | nc -w 1 ocbackup.cs.unibo.it 5000
 
 # Gently run all the processes
 /home/essepuntato/OC/script/gently-run.sh
-
-cd $INITIAL_DIR
