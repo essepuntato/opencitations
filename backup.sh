65dc4dca401112262278c76467450a7dacdaf67b
S <essepuntato@gmail.com>
2016-09-23 16:41:33 +0200

    Updated backup and crontab

--- backup.sh
+++ backup.sh
@@ -30,79 +30,97 @@ done
 # Backup of the OCC
 
 # ar
+# backup data
 CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ar' &"
 eval $CMD
 sleep 2
-# backup data
 dar -c - -R /srv/oc/corpus/ar/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 # backup provenance
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ar_prov' &"
+eval $CMD
+sleep 2
 dar -c - -R /srv/oc/corpus/ar/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
 # be
+# backup data
 CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_be' &"
 eval $CMD
 sleep 2
-# backup data
 dar -c - -R /srv/oc/corpus/be/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 # backup provenance
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_be_prov' &"
+eval $CMD
+sleep 2
 dar -c - -R /srv/oc/corpus/be/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
 # br
+# backup data
 CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_br' &"
 eval $CMD
 sleep 2
-# backup data
 dar -c - -R /srv/oc/corpus/br/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 # backup provenance
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_br_prov' &"
+eval $CMD
+sleep 2
 dar -c - -R /srv/oc/corpus/br/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
 # id
+# backup data
 CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_id' &"
 eval $CMD
 sleep 2
-# backup data
 dar -c - -R /srv/oc/corpus/id/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 # backup provenance
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_id_prov' &"
+eval $CMD
+sleep 2
 dar -c - -R /srv/oc/corpus/id/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
 # ra
+# backup data
 CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ra' &"
 eval $CMD
 sleep 2
-# backup data
 dar -c - -R /srv/oc/corpus/ra/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 # backup provenance
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_ra_prov' &"
+eval $CMD
+sleep 2
 dar -c - -R /srv/oc/corpus/ra/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
 # re
+# backup data
 CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_re' &"
 eval $CMD
 sleep 2
-# backup data
 dar -c - -R /srv/oc/corpus/re/ -X index.json -ar -P "^[0-9]+/[0-9]+$" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 # backup provenance
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_re_prov' &"
+eval $CMD
+sleep 2
 dar -c - -R /srv/oc/corpus/re/ -I "[a-z]*.json" | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
 # prov
-CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-prov_corpus' &"
+CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-corpus_prov' &"
 eval $CMD
 sleep 2
 dar -c - -R prov | nc -w 1 ocbackup.cs.unibo.it 5000
 sleep 3
 
-# references (done and todo)
+# references
 CMD="ssh ocbackup.cs.unibo.it -l oc -t -t 'nc -l -p 5000 | dar_xform -s 500M -w -n - /mnt/backup/oc/$BACKUP_DATE-ref' &"
 eval $CMD
 sleep 2
