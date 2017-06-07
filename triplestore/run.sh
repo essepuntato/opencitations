e364beb995431d2b4e02f40f669d44052fd758c5
S <essepuntato@gmail.com>
2017-06-07 15:04:23 +0200

    Added a timeout constraint to triplestore check

--- triplestore/run.sh
+++ triplestore/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-myv=`curl -s http://localhost:3000/blazegraph/sparql?query=SELECT%20%3Fs%20%7B%3Fs%20%3Fp%20%3Fo%7D%20LIMIT%201`
+myv=`timeout 30s curl -s http://localhost:3000/blazegraph/sparql?query=SELECT%20%3Fs%20%7B%3Fs%20%3Fp%20%3Fo%7D%20LIMIT%201`
 
 if [ -z "$myv" ] || [[ $myv != *"results"* ]];
 then
